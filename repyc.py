6f93fa7e73ba070c79989e6a1901a6421485df4b
Alexandre PIGNÉ <alex@freesenses.net>
2012-04-16 20:29:19 +0200

    Basic instruction name-only disassembler

--- repyc.py
+++ repyc.py
@@ -1,6 +1,42 @@
 #!/usr/bin/env python
-
+import dis
+import marshal
+import opcode
 import optparse
+import struct
+import time as _time
+
+class Repyc(object):
+    """
+    """
+    def __init__(self, bfile, sfile=None, options=None):
+        if not options:
+            options = {}
+        if isinstance(bfile, basestring):
+            bfile = file(bfile, 'rb')
+        data = bfile.read(8)
+        self._magic = ''.join(struct.unpack("cccc", data[:4]))
+        self._time = _time.gmtime(struct.unpack("<i", data[4:])[0])
+        self._bc = marshal.load(bfile)
+        if options.get('verbose'):
+            print "MAGIC:", self._magic.encode('hex')
+            print "TIME:",  _time.asctime(self._time)
+
+    def show_hex(self):
+        print map(ord, self._bc.co_code)
+        return self
+
+    def disassemble(self):
+        i = 0
+        c = map(ord, self._bc.co_code)
+        while i < len(c):
+          inst = c[i]
+          print dis.opname[inst]
+          if inst < opcode.HAVE_ARGUMENT:
+              i += 1
+          else:
+              i += 3
+        return self
 
 def build_parser():
     """
@@ -13,4 +49,5 @@ def build_parser():
     return parser
 
 if __name__ == '__main__':
-    (option, args) = build_parser().parse_args()
+    (options, args) = build_parser().parse_args()
+    Repyc(*args[:2], options=vars(options)).show_hex().disassemble()
