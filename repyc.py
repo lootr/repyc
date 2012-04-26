88a244bb40b004cd61aea57cc650f3e208e827ab
Alexandre PIGNÉ <alex@freesenses.net>
2012-04-26 22:58:01 +0200

    Merge

--- repyc.py
+++ repyc.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import marshal
 import optparse
 import os.path
@@ -6,21 +7,23 @@ import struct
 import sys
 import time as _time
 
+if sys.version_info >= (3,):
+    basestring = str
+
 class Repyc(object):
-    """
-    """
+
     def __init__(self, bfile, sfile=None, options=None):
         if not options:
             options = {}
         if isinstance(bfile, basestring):
-            bfile = file(bfile, 'rb')
+            bfile = open(bfile, 'rb')
         data = bfile.read(8)
         self._magic = struct.unpack("<H", data[:2])[0]
         self._time = _time.gmtime(struct.unpack("<i", data[4:])[0])
         self._bc = marshal.load(bfile)
         if options.get('verbose'):
-            print "MAGIC:", self._magic
-            print "TIME:",  _time.asctime(self._time)
+            print("MAGIC:", self._magic)
+            print("TIME:",  _time.asctime(self._time))
         self._target = None
 
     def find_target(self):
@@ -29,7 +32,7 @@ class Repyc(object):
             p = os.path.join(base_path, entry)
             if os.path.isdir(p):
                 try:
-                    f = file(os.path.join(p, "magic"), 'rb')
+                    f = open(os.path.join(p, "magic"), 'rb')
                 except IOError:
                     continue
                 try:
@@ -46,19 +49,43 @@ class Repyc(object):
         return self
 
     def show_hex(self):
-        print map(ord, self._bc.co_code)
+        print(map(ord, self._bc.co_code))
         return self
 
     def disassemble(self):
+        opcode = self._target.opcode
         i = 0
-        c = map(ord, self._bc.co_code)
+        c = list(map(ord, self._bc.co_code))
         while i < len(c):
-          inst = c[i]
-          print self._target.opcode.opname[inst]
-          if inst < self._target.opcode.HAVE_ARGUMENT:
-              i += 1
-          else:
-              i += 3
+            op = c[i]
+            sys.stdout.write(str(i).rjust(2) + ' ' + opcode.opname[op].ljust(25))
+            if op < self._target.opcode.HAVE_ARGUMENT:
+                i += 1
+            else:
+                if op in opcode.hasconst:
+                    store = self._bc.co_consts
+                elif op in opcode.hasfree:
+                    store = self._bc.co_freevars
+                elif op in opcode.hasname:
+                    store = self._bc.co_names
+                elif op in opcode.hasjrel:
+                    store = None
+                    i += 1
+                elif op in opcode.hasjabs:
+                    store = None
+                    i += 1
+                elif op in opcode.haslocal:
+                    store = self._bc.co_varnames
+                else:
+                    store = None
+                if store:
+                    M = 25
+                    arg = store[c[i+1]]
+                    arg = repr(arg)
+                    arg = "%s ... (%d skipped)" % (arg[:M], len(arg)-M) if arg[M:] else arg
+                    sys.stdout.write("(%s)" % arg)
+                i += 3
+            sys.stdout.write('\n')
         return self
 
 def build_parser():
@@ -71,8 +98,10 @@ def build_parser():
                       help="print disassembling information")
     return parser
 
-if __name__ == '__main__':
+def main():
     (options, args) = build_parser().parse_args()
     Repyc(*args[:2], options=vars(options)).find_target()\
-                                           .show_hex()\
-                                           .disassemble()
+        .disassemble()
+
+if __name__ == '__main__':
+    main()
